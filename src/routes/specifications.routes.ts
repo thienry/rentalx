d04fef2eed0a32cdd4d5b657492d3fbe38398822
Thiago Moura <thmoura14@hotmail.com>
2022-04-22 12:34:37 -0300

    Initial commit

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
+
+const specificationsRoutes = Router()
+
+specificationsRoutes.post('/', (req, res) => createSpecificationController.handle(req, res))
+
+export { specificationsRoutes }
