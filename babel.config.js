9cb3acdaf014f221f8a7bf82c3ebffaeda09ea2b
Thiago Moura <thmoura14@hotmail.com>
2022-06-26 22:48:19 -0300

    chore: add babel to transpile in js

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@core': './src/core',
+          '@utils': './src/utils',
+          '@config': './src/config',
+          '@shared': './src/shared',
+          '@modules': './src/modules',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+}
